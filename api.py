1972a7955ff8b514858385eaf25b0534bb2564d2
tooniesnguyen <nnnhan1882002@gmail.com>
2023-12-27 11:14:21 +0100

    update backend

---
+++ api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="192.168.0.100",port=8000)
--- utils/voice_test.py
+++ utils/voice_test.py
@@ -1,10 +1,4 @@
 import speech_recognition as sr
-recognizer = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Listening...")
-    audio = recognizer.listen(source, timeout = 5)
-try:
-    text = recognizer.recognize_google(audio)
-    print("You said:", text)
-except sr.UnknownValueError:
-    print("Sorry, I could not understand.")
+
+for mic in sr.Microphone.list_microphone_names():
+    print(mic)
